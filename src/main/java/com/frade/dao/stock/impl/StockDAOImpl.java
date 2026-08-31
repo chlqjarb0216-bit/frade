da3feefda77225932126c7c29ac51e8e9c841803
chlqjarb0216-bit <chlqjarb0216@gmail.com>
2026-08-31 17:05:29 +0900

    긴급수정 @Repository 추가

--- src/main/java/com/frade/dao/stock/impl/StockDAOImpl.java
+++ src/main/java/com/frade/dao/stock/impl/StockDAOImpl.java
@@ -3,9 +3,12 @@ package com.frade.dao.stock.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.frade.dao.stock.StockDAO;
 import com.frade.dto.stock.StockInfoDTO;
 
+@Repository
 public class StockDAOImpl implements StockDAO {
 
 	@Override
