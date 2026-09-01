ee16ce233e2014f7ce244a023586f0915cf32c1a
Seung-min <peppyksm@gmail.com>
2026-09-01 17:00:04 +0900

    [FIX] DAO 호출 전 검증로직 컨트롤러로 이동

--- src/main/java/com/frade/controller/order/OrderController.java
+++ src/main/java/com/frade/controller/order/OrderController.java
@@ -5,42 +5,58 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.frade.common.order.TradeOptionCommon;
 import com.frade.dto.order.HistoryDTO;
 import com.frade.dto.order.OrderInfoDTO;
 import com.frade.service.order.OrderService;
 
-
 @Controller
 public class OrderController {
-	
+
 	@Autowired
 	OrderService orderService;
-	
+
 	@GetMapping("/stock/trade")
 	public String trade() {
-		
+
 		return "stock/order";
 	}
-	
+
 	@PostMapping("/stock/trade")
 	public String tradeAction(OrderInfoDTO orderInfo) {
-		
+
 		int result = 0;
+
+		String tradeOption = orderInfo.getTradeOption();
+
 		
-		if(orderInfo.getTradeOption().equals("BUY")) {
-			result = orderService.processBuy(orderInfo);
-		}else {
-			result = orderService.processSell(orderInfo);
+		//매수 매도 및 DAO 호출 전 검증
+		if (TradeOptionCommon.valueOf(tradeOption) == (TradeOptionCommon.BUY)) {
+			if (orderInfo.getOrderCount() <= 0) {
+				System.out.println("주문 수량은 1 이상이어야 함.");
+			} else if (orderInfo.getOrderPrice() <= 0) {
+				System.out.println("주문 금액은 1 이상이어야 함.");
+			} else {
+				result = orderService.processBuy(orderInfo);
+			}
 		}
-			
-		if(result == 0) {
+		if (TradeOptionCommon.valueOf(tradeOption) == (TradeOptionCommon.SELL)) {
+			if (orderInfo.getOrderCount() <= 0) {
+				System.out.println("매도수량은 0보다 커야함");
+			} else if (orderInfo.getOrderPrice() <= 0) {
+				System.out.println("매도가격은 0보다 커야함");
+			} else {
+				result = orderService.processSell(orderInfo);
+			}
+		}
+
+		if (result == 0) {
 			System.out.println("거래 실패");
-		}else {
+		} else {
 			System.out.println("거래 성공");
 		}
-		
+
 		return "redirect:/stock/trade";
 	}
-	
 
 }
--- src/main/java/com/frade/service/order/impl/OrderServiceImpl.java
+++ src/main/java/com/frade/service/order/impl/OrderServiceImpl.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.frade.dao.order.OrderDAO;
+import com.frade.dao.portfolio.CashDAO;
+import com.frade.dao.portfolio.HistoryDAO;
+import com.frade.dao.portfolio.PortfolioDAO;
 import com.frade.dto.order.HistoryDTO;
 import com.frade.dto.order.OrderInfoDTO;
 import com.frade.dto.user.PortfolioDTO;
@@ -16,13 +18,17 @@ import com.frade.service.order.OrderService;
 public class OrderServiceImpl implements OrderService {
 
 	@Autowired
-	OrderDAO orderDAO;
+	PortfolioDAO orderDAO;
+	@Autowired
+	CashDAO cashDAO;
+	@Autowired
+	HistoryDAO historyDAO;
 
 	@Override
 	public int processBuy(OrderInfoDTO orderInfo) { // 매수
 
 		// 임시데이터 하드코딩
-		String stockCode = "000660"; // 삼성전자
+//		String stockCode = "000660"; // 삼성전자
 		int userNum = 1;
 
 		UserCashDTO cash = findUserCashByUserNum(userNum);
@@ -31,17 +37,12 @@ public class OrderServiceImpl implements OrderService {
 		if (orderInfo.getOrderPrice() * orderInfo.getOrderCount() > cash.getCash()) {
 			System.out.println("주문 금액이 보유 예치금보다 많음.");
 			return 0;
-		} else if (orderInfo.getOrderCount() <= 0) {
-			System.out.println("주문 수량은 1 이상이어야 함.");
-			return 0;
-		} else if (orderInfo.getOrderPrice() <= 0) {
-			System.out.println("주문 금액은 1 이상이어야 함.");
-			return 0;
 		}
+
 
 		// 거래기록 저장
 		HistoryDTO history = new HistoryDTO();
-		history.setStockCode(stockCode);
+		history.setStockCode(orderInfo.getStockCode());
 		history.setUserNum(userNum);
 		history.setTradePrice(orderInfo.getOrderPrice());
 		history.setTradeCnt(orderInfo.getOrderCount());
@@ -53,15 +54,15 @@ public class OrderServiceImpl implements OrderService {
 
 		// 포트폴리오 업데이트
 		PortfolioDTO portfolio = new PortfolioDTO();
-		if(findUserPortfolioByUserNumAndStockCode(userNum, stockCode) != null) {
-			portfolio = findUserPortfolioByUserNumAndStockCode(userNum, stockCode);
+		if(findUserPortfolioByUserNumAndStockCode(userNum, orderInfo.getStockCode()) != null) {
+			portfolio = findUserPortfolioByUserNumAndStockCode(userNum, orderInfo.getStockCode());
 			portfolio.setUserStockCnt(portfolio.getUserStockCnt() + orderInfo.getOrderCount());
 			portfolio.setUserBuyCost(portfolio.getUserBuyCost() 
 					+ orderInfo.getOrderCount() * orderInfo.getOrderPrice());
 			updateUserPortfolio(portfolio);
 		}else {
 			portfolio.setUserNum(userNum);
-			portfolio.setStockCode(stockCode);
+			portfolio.setStockCode(orderInfo.getStockCode());
 			portfolio.setUserStockCnt(orderInfo.getOrderCount());
 			portfolio.setUserBuyCost(orderInfo.getOrderCount() * orderInfo.getOrderPrice());
 			insertUserPortfolio(portfolio);
@@ -73,28 +74,23 @@ public class OrderServiceImpl implements OrderService {
 	public int processSell(OrderInfoDTO orderInfo) { // 매도
 
 		// 임시데이터 하드코딩
-		String stockCode = "000660"; // 삼성전자
+//		String stockCode = "000660"; // 삼성전자
 		int userNum = 1;
 
-		PortfolioDTO portfolio = findUserPortfolioByUserNumAndStockCode(userNum, stockCode);
+		PortfolioDTO portfolio = findUserPortfolioByUserNumAndStockCode(userNum, orderInfo.getStockCode());
 
 		// 검증
 		if (portfolio == null) {
 			System.out.println("해당 주식을 보유하고 있지 않음");
+			return 0;
 		} else if (orderInfo.getOrderCount() > portfolio.getUserStockCnt()) {
 			System.out.println("매도수량이 보유수량보다 많음");
 			return 0;
-		} else if (orderInfo.getOrderCount() <= 0) {
-			System.out.println("매도수량은 0보다 커야함");
-			return 0;
-		} else if (orderInfo.getOrderPrice() <= 0) {
-			System.out.println("매도가격은 0보다 커야함");
-			return 0;
-		}
+		} 
 
 		// 거래기록 저장
 		HistoryDTO history = new HistoryDTO();
-		history.setStockCode(stockCode);
+		history.setStockCode(orderInfo.getStockCode());
 		history.setUserNum(userNum);
 		history.setTradePrice(orderInfo.getOrderPrice());
 		history.setTradeCnt(orderInfo.getOrderCount() * -1); // 음수로 매도 구분
@@ -110,7 +106,7 @@ public class OrderServiceImpl implements OrderService {
 		portfolio.setUserBuyCost(portfolio.getUserBuyCost() 
 						- orderInfo.getOrderCount() * orderInfo.getOrderPrice());
 		if(portfolio.getUserStockCnt() == 0) {
-			deleteUserPortfolioByUserNumAndStockCode(userNum, stockCode);
+			deleteUserPortfolioByUserNumAndStockCode(userNum, orderInfo.getStockCode());
 		}else {
 			updateUserPortfolio(portfolio);
 		}
@@ -121,59 +117,51 @@ public class OrderServiceImpl implements OrderService {
 //	================DAO==================
 
 //	=============t_portfolio==============
-	@Override
-	public List<PortfolioDTO> findUserPortfolioListByUserNum(int userNum) {
+	private List<PortfolioDTO> findUserPortfolioListByUserNum(int userNum) {
 		List<PortfolioDTO> portfolioList = orderDAO.findUserPortfolioListByUserNum(userNum);
 		return portfolioList;
 	}
 
-	@Override
-	public PortfolioDTO findUserPortfolioByUserNumAndStockCode(int userNum, String stockCode) {
+
+	private PortfolioDTO findUserPortfolioByUserNumAndStockCode(int userNum, String stockCode) {
 		PortfolioDTO portfolio = orderDAO.findUserPortfolioByUserNumAndStockCode(userNum, stockCode);
 		return portfolio;
 	}
 
-	@Override
-	public int insertUserPortfolio(PortfolioDTO portfolio) { // 포트폴리오 저장
+	private int insertUserPortfolio(PortfolioDTO portfolio) { // 포트폴리오 저장
 		int result = orderDAO.insertUserPortfolio(portfolio);
 		return result;
 	}
 
-	@Override
-	public int updateUserPortfolio(PortfolioDTO portfolio) {
+	private int updateUserPortfolio(PortfolioDTO portfolio) {
 		int result = orderDAO.updateUserPortfolio(portfolio);
 		return result;
 	}
 
-	@Override
-	public int deleteUserPortfolioByUserNumAndStockCode(int userNum, String stockCode) {
+	private int deleteUserPortfolioByUserNumAndStockCode(int userNum, String stockCode) {
 		int result = orderDAO.deleteUserPortfolioByUserNumAndStockCode(userNum, stockCode);
 		return result;
 	}
 
 //	=============t_cash==============
-	@Override
-	public UserCashDTO findUserCashByUserNum(int userNum) {
-		UserCashDTO userCash = orderDAO.findUserCashByUserNum(userNum);
+	private UserCashDTO findUserCashByUserNum(int userNum) {
+		UserCashDTO userCash = cashDAO.findUserCashByUserNum(userNum);
 		return userCash;
 	}
 
-	@Override
-	public int updateUserCash(UserCashDTO userCash) { // 변동 현금정도 저장
-		int result = orderDAO.updateUserCash(userCash);
+	private int updateUserCash(UserCashDTO userCash) { // 변동 현금정도 저장
+		int result = cashDAO.updateUserCash(userCash);
 		return result;
 	}
 
 //	=============t_history==============
-	@Override
-	public List<HistoryDTO> findTradeHistoryByUsernum(int userNum) {
-		List<HistoryDTO> history = orderDAO.findTradeHistoryByUserNum(userNum);
+	private List<HistoryDTO> findTradeHistoryByUsernum(int userNum) {
+		List<HistoryDTO> history = historyDAO.findTradeHistoryByUserNum(userNum);
 		return history;
 	}
 
-	@Override
-	public int insertTradeHistory(HistoryDTO history) { // 거래기록 저장
-		int result = orderDAO.insertTradeHistory(history);
+	private int insertTradeHistory(HistoryDTO history) { // 거래기록 저장
+		int result = historyDAO.insertTradeHistory(history);
 		return result;
 	}
 
