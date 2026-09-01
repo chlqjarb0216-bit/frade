8705c89302dd9dbcafb5c844cdef96fedd9b6981
chlqjarb0216-bit <chlqjarb0216@gmail.com>
2026-09-01 09:38:26 +0900

    버퍼에 필요한 메서드추가

--- src/main/java/com/frade/service/stock/StockDataBufferService.java
+++ src/main/java/com/frade/service/stock/StockDataBufferService.java
@@ -1,6 +1,21 @@
 package com.frade.service.stock;
 
+import java.util.List;
+
+import com.frade.dto.stock.StockPriceDTO;
+
 public interface StockDataBufferService {
 	//실시간 데이터 버퍼에 구겨넣기
 	public void enqueueRealtimeData(String data);
+
+	//버퍼에서 데이터를 꺼내 처리후 맵에 반영 및 chartService로 데이터 전달
+	//처리한 데이터의 시간을 기억해두었다가 분이 바뀌면 5초뒤에 이전분의 시간문자열을 큐에 저장
+	//private 메서드로 변경
+	public void processRealtimeData(String stockJsonText);
+
+	//준비완료된 시간문자열을 큐에서 꺼내 해당하는 데이터를 맵에서 찾아 리스트로 반환
+	public List<StockPriceDTO> flushCompleteMinuteBuffer();
+
+	//내부적으로 stockBufferTaskExecutor에게 무한 루프를 돌며 큐를 처리하는 private 메서드 startConsumingWorker()를 올림.
+	public void init();
 }
