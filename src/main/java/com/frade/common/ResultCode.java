fa3e7b6cc476286978d770e8229061c9183fb023
chlqjarb0216-bit <chlqjarb0216@gmail.com>
2026-09-03 14:51:56 +0900

    , 빠져있던거 수정

--- src/main/java/com/frade/common/ResultCode.java
+++ src/main/java/com/frade/common/ResultCode.java
@@ -13,13 +13,13 @@ public enum ResultCode {
 	SUCCESS("suc_001", "성공"), SUC_EMPTY("suc_002", "성공했으나 비어있음"), FAIL("rej_001", "실패"),
 
 	//개인정보 영역
-	DUP_ID("rej_101", "이미 사용 중인 아이디입니다."), DUP_NICK("rej_102", "이미 사용 중인 닉네임입니다."), DUP_EMAIL("rej_103", "이미 사용 중인 이메일입니다."),
-	PW_NOT_MATCH("rej_104", "비밀번호 확인 불일치"),INVALID_PROFILE_FILE("rej_105", "지원하지 않는 프로필 사진 형식입니다."), 
-	CURRENT_PW_NOT_MATCH("rej_106", "현재 비밀번호가 일치하지 않습니다."),PROFILE_FILE_SAVE_FAIL("rej_107","프로필 사진 저장에 실패했습니다."),
-	INVALID_PASSWORD_INPUT("rej_108", "비밀번호를 모두 입력해주세요."),
+	DUP_ID("rej_101", "이미 사용 중인 아이디입니다."), DUP_NICK("rej_102", "이미 사용 중인 닉네임입니다."),
+	DUP_EMAIL("rej_103", "이미 사용 중인 이메일입니다."), PW_NOT_MATCH("rej_104", "비밀번호 확인 불일치"),
+	INVALID_PROFILE_FILE("rej_105", "지원하지 않는 프로필 사진 형식입니다."), CURRENT_PW_NOT_MATCH("rej_106", "현재 비밀번호가 일치하지 않습니다."),
+	PROFILE_FILE_SAVE_FAIL("rej_107", "프로필 사진 저장에 실패했습니다."), INVALID_PASSWORD_INPUT("rej_108", "비밀번호를 모두 입력해주세요."),
 
 	//게시판 영역
-	COM_TEXT_FAIL("rej_200", "최대 글자수는 100자입니다.")
+	COM_TEXT_FAIL("rej_200", "최대 글자수는 100자입니다."),
 
 	//트레이딩 영역
 
