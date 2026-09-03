8ef6ea1f9cd90372f35a0ede7488055af7a74fca
Seung-min <peppyksm@gmail.com>
2026-09-04 01:13:42 +0900

    Transactional 어노테이션 추가 +  현금,포트폴리오 조회 DAO 매서드 ForUpdate로 변경 + 오류에 맞는 Exception 메시지 추가

--- src/main/java/com/frade/service/order/impl/OrderServiceImpl.java
+++ src/main/java/com/frade/service/order/impl/OrderServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.frade.dao.portfolio.CashDAO;
 import com.frade.dao.portfolio.HistoryDAO;
@@ -29,6 +30,7 @@ public class OrderServiceImpl implements OrderService {
 	PortfolioService portfolioService;
 
 	@Override
+	@Transactional
 	public boolean processBuy(OrderInfoDTO orderInfo) { // 매수
 		
 		int totalPrice = orderInfo.getOrderCount() * orderInfo.getOrderPrice();
@@ -36,7 +38,10 @@ public class OrderServiceImpl implements OrderService {
 		//임시 유저넘버
 		int userNum = 1;
 
-		UserCashDTO cash = findUserCashByUserNum(userNum);
+		UserCashDTO cash = findUserCashByUserNumForUpdate(userNum);
+		if (cash == null) {
+			throw new IllegalStateException("사용자 현금 데이터 없음");
+		}
 
 		// 검증
 		if (totalPrice > cash.getCash()) {
@@ -45,20 +50,11 @@ public class OrderServiceImpl implements OrderService {
 		}
 
 
-		// 거래기록 저장
-		HistoryDTO history = new HistoryDTO();
-		history.setStockCode(orderInfo.getStockCode());
-		history.setUserNum(userNum);
-		history.setTradePrice(orderInfo.getOrderPrice());
-		history.setTradeCnt(orderInfo.getOrderCount());
-		insertTradeHistory(history);
-		
 		// 현금정보 저장
-		updateUserCash(userNum, -1 * totalPrice);
-		
-		
-		
-		
+		if (updateUserCash(userNum, -1 * totalPrice) == 0) {
+			throw new IllegalStateException("현금 정보 갱신 실패");
+		}
+
 		// 포트폴리오 merge into
 		PortfolioDTO portfolio = new PortfolioDTO();
 		portfolio.setUserNum(userNum);
@@ -67,13 +63,25 @@ public class OrderServiceImpl implements OrderService {
 		portfolio.setUserBuyCost(orderInfo.getOrderCount() * orderInfo.getOrderPrice());
 		
 
-		portfolioService.updateOrInsertUserPortfolio(portfolio);
-		
+		if (portfolioService.updateOrInsertUserPortfolio(portfolio) == 0) {
+			throw new IllegalStateException("포트폴리오 갱신 실패");
+		}
+
+		// 거래기록 저장
+		HistoryDTO history = new HistoryDTO();
+		history.setStockCode(orderInfo.getStockCode());
+		history.setUserNum(userNum);
+		history.setTradePrice(orderInfo.getOrderPrice());
+		history.setTradeCnt(orderInfo.getOrderCount());
+		if (insertTradeHistory(history) == 0) {
+			throw new IllegalStateException("거래 기록 insert 실패.");
+		}
 
 		return true;
 	}
 
 	@Override
+	@Transactional
 	public boolean processSell(OrderInfoDTO orderInfo) { // 매도
 
 		int totalPrice = orderInfo.getOrderCount() * orderInfo.getOrderPrice();
@@ -81,7 +89,13 @@ public class OrderServiceImpl implements OrderService {
 		//임시 유저넘버
 		int userNum = 1;
 
-		PortfolioDTO portfolio = portfolioService.findUserPortfolioByUserNumAndStockCode(userNum, orderInfo.getStockCode());
+		UserCashDTO cash = findUserCashByUserNumForUpdate(userNum);
+		if (cash == null) {
+			throw new IllegalStateException("사용자 현금 데이터 없음");
+		}
+
+		PortfolioDTO portfolio = portfolioDAO.findUserPortfolioByUserNumAndStockCodeForUpdate(userNum,
+				orderInfo.getStockCode());
 
 		// 검증
 		if (portfolio == null) {
@@ -92,25 +106,33 @@ public class OrderServiceImpl implements OrderService {
 			return false;
 		} 
 
-		// 거래기록 저장
-		HistoryDTO history = new HistoryDTO();
-		history.setStockCode(orderInfo.getStockCode());
-		history.setUserNum(userNum);
-		history.setTradePrice(orderInfo.getOrderPrice());
-		history.setTradeCnt(orderInfo.getOrderCount() * -1); // 음수로 매도 구분
-		insertTradeHistory(history);
-
 		// 현금정보 저장
-		updateUserCash(userNum, totalPrice);
+		if (updateUserCash(userNum, totalPrice) == 0) {
+			throw new IllegalStateException("현금 정보 갱신 실패");
+		}
 
 		// 포트폴리오 업데이트
 		portfolio.setUserStockCnt(portfolio.getUserStockCnt() - orderInfo.getOrderCount());
 		portfolio.setUserBuyCost(portfolio.getUserBuyCost() 
 						- totalPrice);
 		if(portfolio.getUserStockCnt() == 0) {
-			portfolioService.deleteUserPortfolioByUserNumAndStockCode(userNum, orderInfo.getStockCode());
+			if (portfolioService.deleteUserPortfolioByUserNumAndStockCode(userNum, orderInfo.getStockCode()) == 0) {
+				throw new IllegalStateException("포트폴리오 삭제 실패");
+			}
 		}else {
-			portfolioService.updateUserPortfolio(portfolio);
+			if (portfolioService.updateUserPortfolio(portfolio) == 0) {
+				throw new IllegalStateException("포트폴리오 갱신 실패");
+			}
+		}
+
+		// 거래기록 저장
+		HistoryDTO history = new HistoryDTO();
+		history.setStockCode(orderInfo.getStockCode());
+		history.setUserNum(userNum);
+		history.setTradePrice(orderInfo.getOrderPrice());
+		history.setTradeCnt(orderInfo.getOrderCount() * -1); // 음수로 매도 구분
+		if (insertTradeHistory(history) == 0) {
+			throw new IllegalStateException("거래기록 insert 실패");
 		}
 
 		return true;
@@ -118,8 +140,17 @@ public class OrderServiceImpl implements OrderService {
 	
 
 //	=============t_cash==============
-	private UserCashDTO findUserCashByUserNum(int userNum) {
+	
+
+	
+	@Override
+	public UserCashDTO findUserCashByUserNum(int userNum) {
 		UserCashDTO userCash = cashDAO.findUserCashByUserNum(userNum);
+		return userCash;
+	}
+	
+	private UserCashDTO findUserCashByUserNumForUpdate(int userNum) {
+		UserCashDTO userCash = cashDAO.findUserCashByUserNumForUpdate(userNum);
 		return userCash;
 	}
 
@@ -145,5 +176,7 @@ public class OrderServiceImpl implements OrderService {
 		
 		return false;
 	}
+
+	
 
 }
