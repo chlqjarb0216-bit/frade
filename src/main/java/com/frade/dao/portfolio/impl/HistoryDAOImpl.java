05f589d5b6cb7577a35e94444c233e57f31d7ddf
Seung-min <peppyksm@gmail.com>
2026-09-04 12:36:20 +0900

    mapper 테이블별 분리 및 그에 따른 DAO 수정

--- src/main/java/com/frade/dao/portfolio/impl/HistoryDAOImpl.java
+++ src/main/java/com/frade/dao/portfolio/impl/HistoryDAOImpl.java
@@ -18,13 +18,13 @@ public class HistoryDAOImpl implements HistoryDAO {
 //	==========t_history===========
 	@Override
 	public List<HistoryDTO> findTradeHistoryByUserNum(int userNum) {
-	    List<HistoryDTO> history = sqlSessionTemplate.selectList("order_mapper.findTradeHistoryByUserNum", userNum);
+	    List<HistoryDTO> history = sqlSessionTemplate.selectList("history_mapper.findTradeHistoryByUserNum", userNum);
 	    return history;
 	}
 	
 	@Override
 	public int insertTradeHistory(HistoryDTO history) {
-		int result = sqlSessionTemplate.insert("order_mapper.insertTradeHistory", history);
+		int result = sqlSessionTemplate.insert("history_mapper.insertTradeHistory", history);
 		return result;
 	}
 
