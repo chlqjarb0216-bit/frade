4d9b91e343456ea8fcd1a51651c57fe469387b80
wnsgk0509 <rndrnd7qkd@gmail.com>
2026-09-05 15:34:28 +0900

    [FEAT] 게시글 조회 및 총 개수 카운팅 쿼리 추가 [FIX]Comment 서비스 조회 및 카운팅 로직 수정(Post 와 동일한 스타일로 수정)

--- src/main/java/com/frade/dao/community/CommentDAO.java
+++ src/main/java/com/frade/dao/community/CommentDAO.java
@@ -1,8 +1,18 @@
 package com.frade.dao.community;
 
+import java.util.List;
+import java.util.Map;
+
 import com.frade.dto.community.CommentDTO;
 
 public interface CommentDAO {
 
+	// 댓글 저장
 	public int saveComment(CommentDTO comment);
+	
+	// 특정 게시글의 전체 댓글 개수
+	public int selectCommentTotalCount(int postNum);
+	
+	// 특정 게시글의 페이징 처리된 댓글 목록
+	public List<CommentDTO> selectCommentList(Map<String, Object> params);
 }
