53bf0bb61666a695f62dfef5f409571f69ea599a
wnsgk0509 <rndrnd7qkd@gmail.com>
2026-09-05 19:03:11 +0900

    [FIX] 테스트 로그인 제거 및 로그인 기능 활용해서 커뮤니티쪽 기능 수정

---
+++ src/main/java/com/frade/util/LoginManager.java
@@ -0,0 +1,86 @@
+package com.frade.util;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import com.frade.dto.user.UserSessionDTO;
+
+public class LoginManager {
+
+    public static final String SESSION_LOGIN_USER_KEY = "loginUser";
+
+    // 로그인 유저 세션 저장
+    public static void setSessionLoginUser(
+            HttpSession session, UserSessionDTO loginUser) {
+
+        session.setAttribute(SESSION_LOGIN_USER_KEY, loginUser);
+    }
+
+    public static void setSessionLoginUser(
+            HttpServletRequest request, UserSessionDTO loginUser) {
+
+        setSessionLoginUser(request.getSession(), loginUser);
+    }
+
+    // 로그인 유저 가져오기
+    public static UserSessionDTO getLoginUser(HttpSession session) {
+
+        return (UserSessionDTO)
+                session.getAttribute(SESSION_LOGIN_USER_KEY);
+    }
+
+    public static UserSessionDTO getLoginUser(
+            HttpServletRequest request) {
+
+        return getLoginUser(request.getSession());
+    }
+
+    // 로그인 여부 확인
+    public static boolean isLogin(HttpSession session) {
+
+        if(session.getAttribute(SESSION_LOGIN_USER_KEY) != null) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean isLogin(HttpServletRequest request) {
+
+        return isLogin(request.getSession());
+    }
+
+    // 로그아웃
+    public static void logout(HttpSession session) {
+
+        session.invalidate();
+    }
+
+    public static void logout(HttpServletRequest request) {
+
+        logout(request.getSession());
+    }
+    
+    /**
+ 	 * 로그인 유저 번호 가져오기 :로그인이 안되어있으면 -1을 반환한다
+     **/
+    public static int getLoginUserNum(HttpSession session) {
+
+        UserSessionDTO loginUser = getLoginUser(session);
+        
+        if(loginUser == null) {	
+        	return -1;
+        }
+        
+        
+        return loginUser.getUserNum();
+    }
+
+    public static int getLoginUserNum(HttpServletRequest request) {
+
+        return getLoginUserNum(request.getSession());
+    }
+   
+    
+    
+}
