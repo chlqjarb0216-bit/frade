bbdf28d452727fca9d2702f91709412137dc3ff6
chlqjarb0216-bit <chlqjarb0216@gmail.com>
2026-09-06 18:57:43 +0900

    API 차트 1분봉 데이터 요청 추가

--- src/main/java/com/frade/dao/api/KiwoomApiRepository.java
+++ src/main/java/com/frade/dao/api/KiwoomApiRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.frade.dto.api.KiwoomAccessToken;
 import com.frade.dto.api.StockInfoRawDTO;
+import com.frade.dto.stock.StockPriceDTO;
 
 public interface KiwoomApiRepository {
 	public KiwoomAccessToken getOrRefreshAccessToken();
@@ -11,4 +12,8 @@ public interface KiwoomApiRepository {
 	public void revokeToken();
 
 	public List<StockInfoRawDTO> getMarketAllStockInfo();
+
+	public List<StockPriceDTO> getKOSPIChartDataByDay(String dayString);
+
+	public List<StockPriceDTO> getStockChartDataByDay(String stockCode, String dayString);
 }
