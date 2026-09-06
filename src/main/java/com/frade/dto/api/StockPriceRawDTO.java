cd5d687da8d179ff879ed1fc9e36f15d3c5cbe72
chlqjarb0216-bit <chlqjarb0216@gmail.com>
2026-09-06 18:20:10 +0900

    응답 받을 DTO 생성

---
+++ src/main/java/com/frade/dto/api/StockPriceRawDTO.java
@@ -0,0 +1,34 @@
+package com.frade.dto.api;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.frade.dto.stock.StockPriceDTO;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class StockPriceRawDTO {
+	@JsonProperty("cur_prc")
+	int curPrc;
+	@JsonProperty("trde_qty")
+	long trdeQty;
+	@JsonProperty("cntr_tm")
+	@JsonFormat(pattern = "yyyyMMddHHmmss")
+	LocalDateTime cntrTm;
+	@JsonProperty("open_prc")
+	int openPric;
+	@JsonProperty("high_prc")
+	int highPric;
+	@JsonProperty("low_prc")
+	int lowPric;
+
+	public StockPriceDTO toStockPriceDTO(String stockCode) {
+		return new StockPriceDTO(stockCode, cntrTm, Math.abs(openPric), Math.abs(highPric), Math.abs(lowPric),
+				Math.abs(curPrc), Math.abs(trdeQty));
+	}
+}
