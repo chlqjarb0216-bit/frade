ee0512f8ecbb795c7de39f85a7f8b8afa0f3c5f0
chlqjarb0216-bit <chlqjarb0216@gmail.com>
2026-09-08 14:00:22 +0900

    거래 성패 표기

--- src/main/java/com/frade/controller/order/OrderController.java
+++ src/main/java/com/frade/controller/order/OrderController.java
@@ -4,24 +4,21 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.frade.common.order.PriceOptionCommon;
-import com.frade.common.order.TradeOptionCommon;
 import com.frade.dto.order.OrderInfoDTO;
 import com.frade.dto.stock.StockPreviewDTO;
+import com.frade.service.order.OrderService;
+import com.frade.service.portfolio.PortfolioService;
 import com.frade.service.stock.StockService;
 import com.frade.util.LoginManager;
 import com.frade.util.MarketUtil;
 
-import org.springframework.web.bind.annotation.RequestParam;
-import com.frade.dto.user.PortfolioDTO;
-import com.frade.dto.user.UserCashDTO;
-import com.frade.service.order.OrderService;
-import com.frade.service.portfolio.PortfolioService;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 public class OrderController {
 
@@ -33,22 +30,16 @@ public class OrderController {
 	@Autowired
 	StockService stockService;
 
-
-
 	@PostMapping("/stock/trade")
-	public String tradeAction(OrderInfoDTO orderInfo, HttpSession session) {
+	public String tradeAction(OrderInfoDTO orderInfo, HttpSession session, RedirectAttributes rttr) {
 		if (!LoginManager.isLogin(session)) {
 			return "redirect:/user/login";
 		}
-		
-		
-		if(!MarketUtil.isMarketOpenTime()) {
+
+		if (!MarketUtil.isMarketOpenTime()) {
 			return "redirect:/stock";
 		}
-		
-			
-			
-			
+
 		orderInfo.setUserNum(LoginManager.getLoginUserNum(session));
 		if (orderInfo.getStockCode() == null || orderInfo.getStockCode().isBlank()) {
 			return "redirect:/stock";
@@ -61,24 +52,29 @@ public class OrderController {
 
 		if (orderInfo.getPriceOption() == null
 				|| (long) orderInfo.getOrderCount() * orderInfo.getOrderPrice() > Integer.MAX_VALUE) {
+			rttr.addFlashAttribute("tradeResultMsg", "총 주문 금액이 너무 큽니다");
 			return "redirect:/stock/" + stock.getStockCode();
 		}
-		
-		boolean result = orderService.orderAnalyzer(orderInfo);
-		
-		
-		if(orderInfo.getPriceOption() == PriceOptionCommon.MARKETPRICE) {
-			if (result) {
-				System.out.println("거래 성공");
+
+		try {
+			boolean result = orderService.orderAnalyzer(orderInfo);
+
+			if (orderInfo.getPriceOption() == PriceOptionCommon.MARKETPRICE) {
+				if (result) {
+					rttr.addFlashAttribute("tradeResultMsg", "거래 성공");
+				} else {
+					rttr.addFlashAttribute("tradeResultMsg", "거래 실패");
+				}
 			} else {
-				System.out.println("거래 실패");
+				if (result) {
+					rttr.addFlashAttribute("tradeResultMsg", "지정가 거래 성공");
+				} else {
+					rttr.addFlashAttribute("tradeResultMsg", "지정가 거래 실패");
+				}
 			}
-		}else {
-			if (result) {
-				System.out.println("지정가 저장 완료");
-			}else {
-				System.out.println("지정가 저장 실패");
-			}
+		} catch (Exception e) {
+			log.warn("거래 중 에러 발생: {}", e.getMessage());
+			rttr.addFlashAttribute("tradeResultMsg", "거래 실패" + e.getMessage());
 		}
 
 		return "redirect:/stock/" + orderInfo.getStockCode();
